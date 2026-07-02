6d9de81ddc45189513dd9107f06c93220179c7a5
Roma270719 <roma.bokach.670@gmail.com>
2026-07-02 22:09:13 +0200

    .

--- static/js/bootstrap.js
+++ static/js/bootstrap.js
@@ -1,4 +1,4 @@
-/*!
+﻿/*!
  * Bootstrap v4.3.1 (https://getbootstrap.com/)
  * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
@@ -5244,4 +5244,3 @@
 
     Object.defineProperty(exports, "__esModule", { value: true });
 });
-//# sourceMappingURL=bootstrap.js.map
